d944430ee7faf9b6c511575e373348734fd38b3c
Aditya Pratap <adityapratap7344@gmail.com>
2019-11-10 10:23:19 +0530

    Automated the creation of songs collection in the firebase database instead of manually adding each song one by one

--- src/App.js
+++ src/App.js
@@ -8,39 +8,37 @@ import Navbar from "./components/navbar/navbar.component";
 import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.page";
 import AccountPage from "./pages/account/account.page";
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument /*, createSongsCollection*/ } from "./firebase/firebase.utils";
 
-import { connect } from 'react-redux';
-import { setCurrentUser, setAuthStateFetched } from './redux/user/user.actions';
+import { connect } from "react-redux";
+import { setCurrentUser, setAuthStateFetched } from "./redux/user/user.actions";
 
+//Not needed any more as we have moved the songs data in the firestore
+//import { songsList } from './songsData';
 
 class App extends React.Component {
- 
-
- unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-
-     
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-         
+
         userRef.onSnapshot(snapShot => {
-
           this.props.setAuthStateFetched(true);
-         this.props.setCurrentUser({
-              id: snapShot.id,
-              photoURL:userAuth.photoURL,
-              ...snapShot.data()
+          this.props.setCurrentUser({
+            id: snapShot.id,
+            photoURL: userAuth.photoURL,
+            ...snapShot.data()
           });
-
         });
-        
       }
       this.props.setAuthStateFetched(true);
       this.props.setCurrentUser(null);
     });
+
+    //This was used to automate the  creation of songs collection in firestore 
+    //createSongsCollection(songsList);
   }
 
   componentWillUnmount() {
@@ -49,7 +47,6 @@ class App extends React.Component {
   }
 
   render() {
-
     const { currentUser } = this.props;
 
     return (
@@ -59,18 +56,12 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route
             path="/account"
-            render={() =>
-              currentUser ? <AccountPage /> : <Redirect to="/" />
-            }
+            render={() => (currentUser ? <AccountPage /> : <Redirect to="/" />)}
           />
           <Route
             path="/signin"
             render={() =>
-              currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
             }
           />
         </Switch>
@@ -79,15 +70,15 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
-})
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
-  setAuthStateFetched: hasBeenFetched => dispatch(setAuthStateFetched(hasBeenFetched)),
-})
-
+  setAuthStateFetched: hasBeenFetched =>
+    dispatch(setAuthStateFetched(hasBeenFetched))
+});
 
 export default connect(
   mapStateToProps,
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,54 +1,71 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
-import 'firebase/firestore';
-import 'firebase/auth';
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyCmiVvYtbsugFuJbM3EPYFPnM5jy8RpVBk",
-    authDomain: "musica-d35e9.firebaseapp.com",
-    databaseURL: "https://musica-d35e9.firebaseio.com",
-    projectId: "musica-d35e9",
-    storageBucket: "musica-d35e9.appspot.com",
-    messagingSenderId: "68853528751",
-    appId: "1:68853528751:web:eaad15691ae0b0327d3c5a",
-    measurementId: "G-3C7QEFBW5D"
+  apiKey: "AIzaSyCmiVvYtbsugFuJbM3EPYFPnM5jy8RpVBk",
+  authDomain: "musica-d35e9.firebaseapp.com",
+  databaseURL: "https://musica-d35e9.firebaseio.com",
+  projectId: "musica-d35e9",
+  storageBucket: "musica-d35e9.appspot.com",
+  messagingSenderId: "68853528751",
+  appId: "1:68853528751:web:eaad15691ae0b0327d3c5a",
+  measurementId: "G-3C7QEFBW5D"
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if(!userAuth) return;
+  if (!userAuth) return;
 
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-    const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-    if(!snapShot.exists){
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-
-        try{
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch(error) {
-            console.log('Error creating user', error.message);
-        }
-
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
     }
+  }
 
-    return userRef;
-}
+  return userRef;
+};
+
+export const createSongsCollection = async songsArray => {
+  const songsCollectionRef = firestore.collection("songs");
+  console.log("SongsCollection: ", songsCollectionRef);
+
+  const batch = firestore.batch();
+
+  songsArray.forEach(songObj => {
+    const newSongDocRef = songsCollectionRef.doc();
+    const randomNum = Math.random() * 60;
+    const cost = Math.round(randomNum) * 10;//generating random prices as per the instruction
+    batch.set(newSongDocRef, {
+      ...songObj,
+      price: cost>=200? cost : 200
+    });
+  });
+
+  return await batch.commit();
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
